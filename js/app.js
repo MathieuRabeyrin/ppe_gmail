5edcd3057e2ab87212b958eed62a48ecae8096d9
MathieuRabeyrin <m.rabeyrin0@gmail.com>
2021-03-04 14:35:33 +0100

    update asset

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btnIcon = document.querySelector('nav > a');
 
-
-  const rotateIcon = (className, link) => {
-    btnIcon.href = link;
-    btnIcon.firstChild.classList.toggle('rotation');
-  }
-
   window.addEventListener('scroll', () => {
     if (window.innerHeight + Math.ceil(window.pageYOffset) >= document.body.offsetHeight) {
       btnIcon.firstChild.classList.add('rotation');
