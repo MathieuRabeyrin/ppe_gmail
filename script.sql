71eb0d5452aa36f20819d2a10f72a1583e3e19c9
MathieuRabeyrin <mrabeyrin0@gmail.com>
2021-03-15 22:46:12 +0100

    update sql

--- script.sql
+++ script.sql
@@ -10,5 +10,6 @@ CREATE TABLE user_mail (
        prenom VARCHAR(255) NOT NULL,
        mail VARCHAR(255) NOT NULL,
        password VARCHAR(255) NOT NULL,
-       PRIMARY KEY(id)
+       PRIMARY KEY(id),
+       UNIQUE(mail)
 );
